dd72ba23886b4f167986ae7b7d24120a0b50bd51
alexambrogi <alexambrogi@gmail.com>
2026-03-24 17:09:28 +0100

    add bug to assembly marker to test sonarqube analysis

--- src/Application/AssemblyMarker.cs
+++ src/Application/AssemblyMarker.cs
@@ -4,7 +4,10 @@ using System.Text;
 
 namespace CQRS.POC.Application
 {
+    
     public sealed  class AssemblyMarker
     {
+
+        private string sonar_qube_string_test = "sonarqube-testing";
     }
 }
